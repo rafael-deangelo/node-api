a711ad98fb11eec125f1e2c802e7f15a0a065f45
rafael-deangelo <rafaelangelo15@hotmail.com>
2024-04-02 18:28:01 -0300

    new observations

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -2,7 +2,6 @@ const express = require('express');
 const Produto = require('../models/produto');
 
 class ProdutoController {
-
     /*
     Esta classe é um controlador de produtos, responsável por acessar o banco de dados e retornar para quem requisitar,
     sendo possível realizar as seguintes operações:
@@ -14,7 +13,6 @@ class ProdutoController {
         - Excluir produtos pelo seu ID.
     Em suma, essa classe gerencia todas as operações relacionadas aos produtos, facilitando o acesso e a manipulação desses dados.
     */
-
 
     // O req.params.id é o valor definido na rota '/:id' o que diz que o atributo 'id' é um paremetro definido na URL
     
@@ -86,11 +84,11 @@ class ProdutoController {
     }
 
     // Método para listar produtos com base em filtros
-    async listarPorFiltros(req, res) {
+    async listarPorFiltros(req, res) { // Recebe 2 parametros | Req: HTTP feita pelo cliente | Res: HTTP enviado de volta ao cliente
         try {
-            const { nome, empresa, descricao, quantidade, marca, valor } = req.body;
-            const filtros = {};
-            // Atributos do modelo Produto
+            const { nome, empresa, descricao, quantidade, marca, valor } = req.body; // Extrai os filtros de busca do corpo da req.
+            const filtros = {};  // Cria um objeto vazio que vai ser preenchido com os filtros
+            // Atributos do modelo Produto 
             if (nome) filtros.nome = nome;
             if (empresa) filtros.empresa = empresa;
             if (descricao) filtros.descricao = descricao;
@@ -98,37 +96,37 @@ class ProdutoController {
             if (marca) filtros.marca = marca;
             if (valor) filtros.valor = valor;
 
-            // O método find() do Mongoose busca os produtos com base nos filtros mandados como parâmetro, que nesse caso são os valores definidos no JSON filtro para puxar todos.
+            // Esta linha usa o método find() do Mongoose para buscar produtos no banco de dados MongoDB com base nos filtros definidos, e armazena na variavel resultado
             const resultado = await Produto.find(filtros);
-            if(resultado.length> 0){
-                res.status(200).json(resultado);
+            if(resultado.length> 0){   // aqui vai verificar se a consulta retornou algum produto
+                res.status(200).json(resultado); // Se sim envia uma resposta HTTP com status OK e os produtos no formato JSON
             }else{
-                res.status(404).json("Nenhum produto encontrado com esse filtro");
+                res.status(404).json("Nenhum produto encontrado com esse filtro"); // Se não envia uma resposta NOT FOUND 
             };
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Erro ao listar os produtos por filtros' });
+            res.status(500).json({ error: 'Erro ao listar os produtos por filtros' }); // Aqui caso ocorra algum erro retorna um 500 Internal Server Error
         }
     }
 
     // Método para excluir um produto pelo seu ID
-    async excluir(req, res) {
+    async excluir(req, res) {  // Recebe 2 parametros | Req: HTTP feita pelo cliente | Res: HTTP enviado de volta ao cliente
         try {
             const id = req.params.id;
             // O método findByIdAndRemove() do Mongoose busca um produto pelo seu ID e o remove do MongoDB.
             // Recebe como argumento o ID do produto a ser removido.
             // Retorna uma promessa (Promise) que é resolvida com o produto removido em caso de sucesso.
-            const resultado = await Produto.findByIdAndRemove(id);
+            const resultado = await Produto.findByIdAndRemove(id); // Aqui ele busca e remove o produto do banco com base no ID fornecido e retorna uma promessa (Promise), que é resolvida com o produto removido em caso de sucesso.
             if (resultado) {
-                res.status(200).send();
+                res.status(200).send(); // Verifica se o resultado da operação de exclusão é válido e retorna OK
             } else {
-                res.status(404).json({ error: 'Produto não encontrado' });
+                res.status(404).json({ error: 'Produto não encontrado' }); // Caso contrario um NotFound
             }
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Erro ao excluir o produto' });
+            res.status(500).json({ error: 'Erro ao excluir o produto' }); // E por ultimo se houver um erro retorna um 500 (Internal Server Error)
         }
     }
 }
 
-module.exports = new ProdutoController();
+module.exports = new ProdutoController(); // Exporta a instancia do ProdutosController para que possa ser utilizada em outros arquivos do projeto
